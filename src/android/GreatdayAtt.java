ec6e2b49efa3331efb2ff354f134e61d9e4b4304
Tomy Permadi <tomy.permadi@greatdayhr.com>
2019-08-06 15:53:03 +0700

    sdf

--- src/android/GreatdayAtt.java
+++ src/android/GreatdayAtt.java
@@ -12,21 +12,35 @@ import org.json.JSONObject;
  */
 public class GreatdayAtt extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("add".equals(action)) {
-            this.test(args, callbackContext);
-            return true;
+    package cordova.plugin.greatdayatt;
 
-            //            String message = args.getString(0);
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+    /**
+     * This class echoes a string called from JavaScript.
+     */
+    public class GreatdayAtt extends CordovaPlugin {
+
+        @Override
+        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+            if (action.equals("add")) {
+                this.test(args, callbackContext);
+                return true;
+
+                //            String message = args.getString(0);
 //            this.coolMethod(message, callbackContext);
 //            return true;
-        }else if ("test".equals(action) ){
-            this.test(args, callbackContext);
-            return true;
+            }else if (action.equals("test") ){
+                this.test(args, callbackContext);
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
 //    private void coolMethod(String message, CallbackContext callbackContext) {
 //        if (message != null && message.length() > 0) {
@@ -36,31 +50,35 @@ public class GreatdayAtt extends CordovaPlugin {
 //        }
 //    }
 
-    private void add(JSONArray args, CallbackContext callback) {
-        if (args != null) {
-            try {
-                int p1 = Integer.parse(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parse(args.getJSONObject(0).getString("param2"));
-                callback.success("" + (p1 + p2));
-            } catch (Exception ex) {
-                callback.error("something wrong" + ex)
+        private void add(JSONArray args, CallbackContext callback) {
+            if (args != null) {
+                try {
+//                int p1 = Integer.parse(args.getJSONObject(0).getString("param1"));
+//                int p2 = Integer.parse(args.getJSONObject(0).getString("param2"));
+                    int p1 = 1;
+                    int p2 = 4;
+                    callback.success("" + (p1 + p2));
+                } catch (Exception ex) {
+                    callback.error("something wrong" + ex);
+                }
+            } else {
+                callback.error("not value");
             }
-        } else {
-            callback.error("not value")
+        }
+
+        private void test(JSONArray args, CallbackContext callback) {
+            if (args != null) {
+                try {
+                    int p1 = 1;
+                    int p2 = 2;
+                    callback.success("" + (p1 + p2));
+                } catch (Exception ex) {
+                    callback.error("something wrong" + ex);
+                }
+            } else {
+                callback.error("not value");
+            }
         }
     }
 
-    private void test(JSONArray args, CallbackContext callback) {
-        if (args != null) {
-            try {
-                int p1 = Integer.parse(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parse(args.getJSONObject(0).getString("param2"));
-                callback.success("" + (p1 + p2));
-            } catch (Exception ex) {
-                callback.error("something wrong" + ex)
-            }
-        } else {
-            callback.error("not value")
-        }
-    }
 }
